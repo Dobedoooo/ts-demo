7bb13c20af80c72b5831da8bba6ad897a98e1a7d
dobedoooo <1114170770@qq.com>
2021-01-27 16:51:36 +0800

    default

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { store } from './store/index';
 
-createApp(App).mount('#app')
+// import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).use(router).use(store).mount('#app');
---
+++ src/views/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="">
+        <HelloWorld msg="nihao"/>
+    </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+export default {
+    props: {
+    },
+    data () {
+        return {
+        }
+    },
+    created () {},
+    mounted () {},
+    watch: {},
+    computed: {},
+    methods: {},
+    components: {
+        HelloWorld,
+    },
+}
+</script>
+<style scoped>
+</style>
